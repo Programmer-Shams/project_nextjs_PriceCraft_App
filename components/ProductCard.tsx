219513363a6f808cf3c8e9b7c871b2d7b2be4000
ShamsTech005 <ssdeen313@gmail.com>
2023-12-28 01:02:51 +0100

    final commit

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,24 +1,36 @@
-import { Product } from "@/types"
-import Image from "next/image"
-import Link from "next/link"
+import { Product } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
-    product: Product
-}
-const ProductCard = ({product}:Props ) => {
+  product: Product;
+};
+const ProductCard = ({ product }: Props) => {
   return (
-    <Link href={`/products/${product._id}`} className="product-card">
-        <div className="product-card_img-container">
-            <Image src={product.image} alt={product.title} className="product-card_img" height={200} width={200}/>
-            <div className="flex flex-col gap-3">
-            <h3 className="product-title">{product.title}</h3>
-            <div className="flex">
-
-            </div>
-            </div>
+    <Link href={`/product/${product._id}`} className="product-card">
+      <div className="product-card_img-container">
+        <Image
+          src={product.image}
+          alt={product.title}
+          className="product-card_img"
+          height={200}
+          width={200}
+        />
+        <div className="flex flex-col gap-3">
+          <h3 className="product-title">{product.title}</h3>
+          <div className="flex justify-between">
+            <p className="text-blabk opacity-50 text-lg capitalize">
+              {product.category}
+            </p>
+            <p className="text-black text-lg font-semibold">
+              <span>{product?.currency}</span>
+              <span>{product?.currentPrice}</span>
+            </p>
+          </div>
         </div>
+      </div>
     </Link>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -6,6 +6,8 @@ import { connectToDatabase } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 import { revalidatePath } from "next/cache";
+import { User } from "@/types";
+import { generateEmailBody, sendEmail } from "../nodemailer";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
   if (!productUrl) return;
@@ -31,10 +33,10 @@ export async function scrapeAndStoreProduct(productUrl: string) {
       };
     }
     const newProduct = await Product.findOneAndUpdate(
-      {url: scrapeProduct.url},
+      { url: scrapeProduct.url },
       product,
       { new: true, upsert: true }
-    )
+    );
     revalidatePath(`/products/${newProduct._id}`);
   } catch (error: any) {
     throw new Error(`Faild to create/update product: ${error.message}`);
@@ -42,21 +44,59 @@ export async function scrapeAndStoreProduct(productUrl: string) {
 }
 export async function getProductById(productId: string) {
   try {
-    connectToDatabase()
-    const product = await Product.findById({ _id: productId })
-    if(!product) return;
-    return product
+    connectToDatabase();
+    const product = await Product.findById({ _id: productId });
+    if (!product) return;
+    return product;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 export async function getAllProducts() {
   try {
-    connectToDatabase()
-    const products = await Product.find()
-    if(!products) return;
-    return products
+    connectToDatabase();
+    const products = await Product.find();
+    if (!products) return;
+    return products;
   } catch (error) {
-    console.log(error)
+    console.log(error);
+  }
+}
+export async function getSimilarProducts(productId: string) {
+  try {
+    connectToDatabase();
+
+    const currentProduct = await Product.findById(productId);
+
+    if (!currentProduct) return null;
+
+    const similarProducts = await Product.find({
+      _id: { $ne: productId },
+    }).limit(3);
+
+    return similarProducts;
+  } catch (error) {
+    console.log(error);
+  }
+}
+export async function addUserEmailToProduct(productId: string, userEmail: string) {
+  try {
+    const product = await Product.findById(productId);
+
+    if(!product) return;
+
+    const userExists = product.users.some((user: User) => user.email === userEmail);
+
+    if(!userExists) {
+      product.users.push({ email: userEmail });
+
+      await product.save();
+
+      const emailContent = await generateEmailBody(product, "WELCOME");
+
+      await sendEmail(emailContent, [userEmail]);
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
